aa812801535d33b5cc0f62d92559ac1e61ab815b
Davi Pereira dos Santos <davi.solnassente@gmail.com>
2026-05-09 22:38:50 -0300

    feat✨:Ativando o auth interceptor.

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -2,6 +2,8 @@ import { ApplicationConfig, LOCALE_ID, provideBrowserGlobalErrorListeners, provi
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from './core/interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -9,5 +11,8 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes, withComponentInputBinding()),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideAnimations(),
+    provideHttpClient(
+      withInterceptors([authInterceptor])
+    )
   ]
 };
